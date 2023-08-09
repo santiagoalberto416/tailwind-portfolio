480eecd4021df4247483a9b7de45b471d6ba36ed
Santiago Alberto Kirk Cabrera <santiagoalberto416@gmail.com>
2023-08-08 21:22:53 -0700

    Return animation is now added

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,7 +3,8 @@ import Hamburger from "@/icons/hamburger";
 import { FC, useState } from "react";
 
 const Header: FC<{}> = () => {
-  const [show, setShow] = useState(false);
+  const [show, setShow] = useState<undefined | boolean>(undefined);
+  const className = show === undefined ? "" : show ? "slide-in" : "slide-out";
   return (
     <nav className="w-100">
       <div className="flex w-100 p-4 shadow-lg text-center">
@@ -21,9 +22,7 @@ const Header: FC<{}> = () => {
       </div>
 
       <div
-        className={`w-full ${
-          show ? "block" : "hidden"
-        } h-screen bg-white z-10 lg:hidden flex flex-col center shadow-lg absolute top-0`}
+        className={`w-full mobile-menu ${className} h-screen bg-white z-10 lg:hidden flex flex-col center shadow-lg absolute top-0`}
       >
         <div className="w-100 flex justify-end p-4">
           <Close onClick={() => setShow(false)} />
