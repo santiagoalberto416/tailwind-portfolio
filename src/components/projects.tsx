5c38fb26c3c52eafd6af19e124f61ce39be6dea1
Santiago Alberto Kirk Cabrera <santiagoalberto416@gmail.com>
2024-05-23 18:43:45 -0700

    Now you can see images at full screen

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -3,15 +3,65 @@ import { R2_BUCKET } from "@/utils/resources";
 import Link from "next/link";
 import { SectionsIds } from "@/components/header";
 
+import {
+  Description,
+  Dialog,
+  DialogPanel,
+  DialogTitle,
+  Transition,
+} from "@headlessui/react";
+import { useState } from "react";
+
+function Example() {
+  let [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <>
+      <button onClick={() => setIsOpen(true)}>Open dialog</button>
+      <Dialog
+        open={isOpen}
+        onClose={() => setIsOpen(false)}
+        className="relative z-50"
+      >
+        {/* The backdrop, rendered as a fixed sibling to the panel container */}
+        <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
+
+        {/* Full-screen container to center the panel */}
+        <div className="fixed inset-0 flex w-screen items-center justify-center p-4">
+          {/* The actual dialog panel  */}
+          <DialogPanel className="max-w-lg space-y-4 bg-white p-12">
+            <DialogTitle className="font-bold">Deactivate account</DialogTitle>
+            <Description>
+              This will permanently deactivate your account
+            </Description>
+            <p>
+              Are you sure you want to deactivate your account? All of your data
+              will be permanently removed.
+            </p>
+            <div className="flex gap-4">
+              <button onClick={() => setIsOpen(false)}>Cancel</button>
+              <button onClick={() => setIsOpen(false)}>Deactivate</button>
+            </div>
+          </DialogPanel>
+        </div>
+      </Dialog>
+    </>
+  );
+}
+
 type Project = {
   title: string;
   description: string;
   image: string;
+  onClickImage?: () => void;
   link: {
     path: string;
     text: string;
   };
 };
+
+const description =
+  "Here are some of my personal projects I've worked on recently.";
 
 const CurrentProjects: Project[] = [
   {
@@ -41,7 +91,13 @@ const CurrentProjects: Project[] = [
   },
 ];
 
-const Project: FC<Project> = ({ title, description, image, link }) => (
+const Project: FC<Project> = ({
+  title,
+  description,
+  image,
+  link,
+  onClickImage,
+}) => (
   <div className="project-card flex lg:flex-row flex-col-reverse  p-4 rounded gap-5 items-center lg:mx-auto mx-3 ">
     <div className="flex flex-col gap-4 text-white text-section">
       <h2 className="text-2xl">{title}</h2>
@@ -54,21 +110,73 @@ const Project: FC<Project> = ({ title, description, image, link }) => (
         </Link>
       </div>
     </div>
-    <img className="rounded" src={R2_BUCKET + "/" + image} alt={title} />
+
+    <div
+      className="image-section relative"
+      style={{ minWidth: "240px" }}
+      onClick={onClickImage}
+    >
+      <img
+        className="rounded screenshot"
+        src={R2_BUCKET + "/" + image}
+        alt={title}
+      />
+    </div>
   </div>
 );
 
 const Projects: FC<{ projects?: Project[] }> = ({
   projects = CurrentProjects,
-}) => (
-  <div id={SectionsIds.Projects}>
-    <div className="projects container mx-auto pb-4 flex flex-col gap-4">
-      <h1 className="text-4xl text-white text-center mb-10">Projects</h1>
-      {projects.map((project) => (
-        <Project {...project} key={project.title} />
-      ))}
+}) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedImage, setSelectedImage] = useState<string>("");
+
+  return (
+    <div id={SectionsIds.Projects}>
+      <div className="projects container mx-auto pb-4 flex flex-col gap-4">
+        <h1 className="text-4xl text-white text-center mb-10">Projects</h1>
+        <Transition
+          show={isOpen}
+          enter="duration-200 ease-out"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="duration-300 ease-out"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <Dialog
+            open={isOpen}
+            onClose={() => setIsOpen(false)}
+            className="relative z-50"
+          >
+            {/* The backdrop, rendered as a fixed sibling to the panel container */}
+            <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
+
+            {/* Full-screen container to center the panel */}
+            <div className="fixed inset-0 flex w-screen items-center justify-center p-4">
+              {/* The actual dialog panel  */}
+              <DialogPanel className="max-w-screen-lg space-y-4 bg-black p-12">
+                <img src={R2_BUCKET + "/" + selectedImage} alt="Selected" />
+              </DialogPanel>
+            </div>
+          </Dialog>
+        </Transition>
+        <p className="max-w-3xl text-center mx-auto text-white mb-10">
+          {description}
+        </p>
+        {projects.map((project) => (
+          <Project
+            {...project}
+            key={project.title}
+            onClickImage={() => {
+              setIsOpen(true);
+              setSelectedImage(project.image);
+            }}
+          />
+        ))}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Projects;
