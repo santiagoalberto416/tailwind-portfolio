7e58bfacccd61fa743446062864bc030399accbf
Santiago Alberto Kirk Cabrera <santiagoalberto416@gmail.com>
2024-05-28 11:51:19 -0700

    Fixed style for contact section

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -1,5 +1,6 @@
 // components/EmailForm.tsx
-import { useState, FormEvent } from "react";
+import React, { useState, FormEvent } from "react";
+import { SectionsIds } from "@/components/header";
 
 export default function Contact() {
   const [to, setTo] = useState("");
@@ -22,35 +23,114 @@ export default function Contact() {
     alert(result.message);
   };
 
+  const inputClass = "p-2 rounded w-full";
+  const inputLabelClass = "min-w-36 text-white pb-2";
+  const inputContainerClass = "flex flex-col w-full ";
+
+  const contactButtons = [
+    {
+      href: "https://www.instagram.com/santiagokirk",
+      imgSrc: "/outline-insta-icon.svg",
+      altText: "Instagram Icon",
+      text: "@santiagokirk",
+      title: "Instagram",
+      className: "",
+    },
+    {
+      href: "https://www.facebook.com/santiagoalberto.kirkcabrera/",
+      imgSrc: "/outline-facebook-icon.svg",
+      altText: "Facebook Icon",
+      title: "Facebook",
+      text: "santiagoalberto.kirkcabrera",
+    },
+    {
+      href: "mailto:santiagoalberto416@gmail.com",
+      imgSrc: "/outline-email-icon.svg",
+      altText: "Email Icon",
+      title: "Email",
+      text: "santiagoalberto416@gmail.com",
+    },
+  ];
+
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label>To:</label>
-        <input
-          type="email"
-          value={to}
-          onChange={(e) => setTo(e.target.value)}
-          required
-        />
+    <div className="pb-20">
+      <div
+        id={SectionsIds.Contact}
+        className="my-20 rounded contact p-7 w-full grid grid-cols-2 container mx-auto max-w-screen-lg justify-center"
+      >
+        <div className="flex flex-col col-span-1 text-white">
+          <h1 className="text-4xl text-white mb-4">Let&apos;s talk</h1>
+          <p className="text-white mb-0">
+            Feel free to leave a comment about the side, any improvement or
+            anything you want to share
+          </p>
+
+          <div className="flex flex-col gap-2">
+            <h2 className="text-2xl text-white mt-10">Contact me</h2>
+            <p className="text-white">
+              You can also contact me through my social media or email
+            </p>
+            <div className="image-section flex flex-col gap-4">
+              {contactButtons.map((button, index) => (
+                <a
+                  key={index}
+                  className=" rounded pr-4 mr-4"
+                  href={button.href}
+                >
+                  <div className="flex gap-2 items-center">
+                    <div className="bg-blue-900 rounded p-1.5">
+                      <img src={button.imgSrc} alt={button.altText} />
+                    </div>
+                    <div>
+                      <div className="font-bold">{button.title}</div>
+                      <span className="text-gray-200">{button.text}</span>
+                    </div>
+                  </div>
+                </a>
+              ))}
+            </div>
+          </div>
+        </div>
+        <form
+          className="col-span-1 flex flex-col gap-4 p-4 rounded-lg w-full"
+          style={{ backgroundColor: "#000063" }}
+          onSubmit={handleSubmit}
+        >
+          <div className={inputContainerClass}>
+            <label className={inputLabelClass}>Email:</label>
+            <input
+              className={inputClass}
+              placeholder="optional, only if you want reply"
+              type="email"
+              value={to}
+              onChange={(e) => setTo(e.target.value)}
+              required
+            />
+          </div>
+          <div className={inputContainerClass}>
+            <label className={inputLabelClass}>What&apos;s your name:</label>
+            <input
+              className={inputClass}
+              type="text"
+              value={subject}
+              onChange={(e) => setSubject(e.target.value)}
+              required
+            />
+          </div>
+          <div className={`flex flex-col w-full items-start`}>
+            <label className={inputLabelClass}>Message:</label>
+            <textarea
+              className={inputClass + " h-36"}
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+              required
+            />
+          </div>
+          <button className="bg-white p-2 border rounded w-fit" type="submit">
+            Send Email
+          </button>
+        </form>
       </div>
-      <div>
-        <label>Subject:</label>
-        <input
-          type="text"
-          value={subject}
-          onChange={(e) => setSubject(e.target.value)}
-          required
-        />
-      </div>
-      <div>
-        <label>Message:</label>
-        <textarea
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-          required
-        />
-      </div>
-      <button type="submit">Send Email</button>
-    </form>
+    </div>
   );
 }
