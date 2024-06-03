c77ca19900d3c2e3d15f2d1f37ab69f12348588a
Santiago Alberto Kirk Cabrera <santiagoalberto416@gmail.com>
2024-06-02 20:55:11 -0700

    Fixed hero and global style

--- src/components/hero2.tsx
+++ src/components/hero2.tsx
@@ -7,7 +7,7 @@ const Hero2: FC<{}> = () => {
     <div className="transform duration-500 hover:scale-105 lg:mx-0 mx-3">
       <div className="hero-2 w-auto rounded-lg overflow-auto border-0 bg-white shadow-lg">
         <div className="flex items-center lg:flex-row flex-col">
-          <div className="px-7 pt-7">
+          <div className="lg:px-7 lg:pt-7 p-7">
             <h1 className="text-2xl lg:mt-0 lg:text-left text-center mb-2">
               Hobbies
             </h1>
@@ -16,7 +16,7 @@ const Hero2: FC<{}> = () => {
               learning new things and I&apos;m always looking for new
               opportunities to grow.
             </p>
-            <p className="lg:w-80 flex align-bottom  text-left">
+            <p className="lg:w-80 flex align-bottom mb-0 text-left">
               I like challenges and I&apos;m always looking for ways to improve
               my skills and knowledge. And also improve the things that I&apos;m
               part of
@@ -24,10 +24,10 @@ const Hero2: FC<{}> = () => {
           </div>
           <div
             style={{ minWidth: "200px", width: "100%", height: "auto" }}
-            className="d-flex items-center h-full lg:pr-7"
+            className="d-flex items-center h-full lg:p-7"
           >
             <Image
-              className="overflow-clip border-0"
+              className="lg:border lg:rounded-lg lg:overflow-clip border-0 overflow-clip shadow-lg"
               src={R2_BUCKET + "/profile-pic-2.png"}
               width={300}
               height={300}
